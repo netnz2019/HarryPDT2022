d08c1d2a0676e0e0aa4d1dbfcda9da8755289455
harryPrendergast0 <86515165+harryPrendergast0@users.noreply.github.com>
2022-06-09 12:09:22 +1200

    Bug fixing
    Previously the program would get the download by pressing a varity of buttons now however it just request the download via a link and some cookies, this lowered the chance of failure and improved efficacy. The older version had a problem where it would fail to download bring up a 500 error.

--- Booking Download py/vrbo.py
+++ Booking Download py/vrbo.py
@@ -16,51 +16,83 @@ import os
 from requests_html import HTMLSession
 import pandas as pd
 from bs4 import BeautifulSoup
-
+import pickle
+from selenium.webdriver.chrome.options import Options
+from datetime import date
 
 
 def Main():
 
     options = webdriver.ChromeOptions()
+
     options.headless = False
 
-    driver = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\chromedriver.exe', options=options)
 
+    driver = webdriver.Chrome(executable_path=r"C:\Program Files (x86)\chromedriver.exe", options=options)
 
+    def addcookes(cookies):
+        for cookie in cookies:
+            print(cookie)
+            driver.add_cookie(cookie)
 
     def login():
+
+        #file = open("DictFile.pkl", "rb")
+        #file_contents = pickle.load(file)
+        #addcookes(file_contents)
+
+        list_of_files = glob.glob('C:/Users/harry/Downloads/*.csv')
+        lastDown = max(list_of_files, key=os.path.getctime)
         print("Vrbo login....")
         driver.get(geturl(1))
         print("Webpage reached...")
-        addCookies()
+        cookieJar()
 
         credintals()
-        driver.get('https://www.vrbo.com/en-nz/rm/reservations/page/1/sort/stay/asc/filter/all-reservations')
-        download()
+
+        time.sleep(4)
+        cookies = driver.get_cookies()
+        driver.get(geturl(3))
+
+
+        driver.get('https://www.vrbo.com/en-nz/rm/reservations/page/1/sort/stay/asc/filter/reservations')
+        for Cookie in cookies:
+            driver.add_cookie(Cookie)
+        #cookieJar()
         time.sleep(5)
+        download()
+        time.sleep(15)
+        list_of_files = glob.glob('C:/Users/harry/Downloads/*.csv')
+        down = max(list_of_files, key=os.path.getctime)
+
+        if lastDown == down:
+            Main()
         convert()
 
 
 
     def download():
+        dat = date.today()
+        print(dat)
+        driver.get(geturl(3))
 
         dowbutton = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, settings.DOWNBUTVR))
         )
-        time.sleep(0.5)
+        time.sleep(3)
 
         dowbutton.click()
-        time.sleep(0.5)
+        time.sleep(3)
         toggle = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, settings.TOGGLEDVR))
         )
-        time.sleep(0.5)
+
         subbut = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, settings.SUBBUTTON))
         )
-        time.sleep(4)
+        time.sleep(5)
         toggle.click()
-        time.sleep(1)
+        time.sleep(5)
         subbut.click()
 
 
@@ -74,19 +106,20 @@ def Main():
         read_file = pd.read_csv(latest_file)
         read_file.to_excel(r'C:\Users\harry\PycharmProjects\pythonProject10\vrtest.xls', index=None, header=True)
 
-    def addCookies():
+    def cookieJar():
         driver.add_cookie({"name":'bm_sv', "value":'8260E5DD57BDC9729F3E7915B099CD29~YAAQj6lgaFXF4OGAAQAA4+jOAg82a0yOJIWRL1uSed/ZmbZzPwQVHWdhkTr+rq65znk7CT0rYrvTQ+8s/S7+3kTnwMMdiScJPZ/OiUB61KO6evTWV7Ksoc12r5dEwnegMYqlHZeJ1U2iQtIVsJi2ZJ/umG3h5dzTRwhjNSW4eYDv2+jjDhttpA54Ua4U9zgl7mvCDHRUQq6NVoRP9G+r3fm0Wd8KC3UHV44AajO8wO8etw3cHcl2++LHY2RVv40=~1'})
         driver.add_cookie({"name":'_clsk' , "value":'plrei|1653609524595|2|0|b.clarity.ms/collect' })
         driver.add_cookie({"name":'site', "value":'homeaway_nz'})
         driver.add_cookie({"name":'DUAID', "value": '3be56189-fcac-469c-a38e-204d9661703d'})
         driver.add_cookie({"name":'xdid', "value":'981cc0ca-be43-4f70-aa0e-59474726a3ed|1653609492|vrbo.com'})
-        driver.add_cookie({"name":'EG_SESSIONTOKEN', "value":'h5nAD5oYM_UNXdb4Qce9N_4c4tX_09zR5yfnFRq_qEM:SJVNYTYIbTxZrBbiVgk-WAEKReTQITjtn6SYEt1EL2U'})
+        #driver.add_cookie({"name":'EG_SESSIONTOKEN', "value":'h5nAD5oYM_UNXdb4Qce9N_4c4tX_09zR5yfnFRq_qEM:SJVNYTYIbTxZrBbiVgk-WAEKReTQITjtn6SYEt1EL2U'})
         driver.add_cookie({"name":'_gcl_au', "value":'1.1.571117949.1653609490'})
-        driver.add_cookie({"name":'HASESSIONV3', "value":'ae86e28d-dad5-480c-adc8-da0007ce0833'})
+        #driver.add_cookie({"name":'HASESSIONV3', "value":'ae86e28d-dad5-480c-adc8-da0007ce0833'})
         driver.add_cookie({"name":'_uetsid', "value":'b1a5d0e0dd4f11eca5878d3b95522f2d'})
         driver.add_cookie({"name":'_clck', "value":'1eiq1oe|1|f1s|0'})
         driver.add_cookie({"name":'2008a337-b332-5e57-a558-0c88dc48b53fUAL', "value":'1'})
         driver.add_cookie({"name":'_fbp', "value":'fb.1.1653609490476.1119927072'})
+
 
 
 
@@ -107,19 +140,24 @@ def Main():
             EC.presence_of_element_located((By.XPATH, settings.LOGIN_BUTTONVR))
         )
 
+
         login.send_keys(settings.USERNAMEVR)
         password.send_keys(settings.PASSWORDVR)
         login_button.click()
 
-        print(get_cookies())
+        file = open("DictFile.pkl", "wb")
+        pickle.dump(driver.get_cookies(), file)
+        file.close()
 
-    def get_cookies():
-        cookies = {}
-        selenium_cookies = driver.get_cookies()
-        for cookie in selenium_cookies:
-            cookies[cookie['name']] = cookie['value']
+        file = open("DictFile.pkl", "rb")
+        file_contents = pickle.load(file)
+        print(file_contents)
 
-        return cookies
+
+
+
+
+
 
 
     def geturl(number):
@@ -129,6 +167,9 @@ def Main():
         elif number == 2:
             return 'https://admin.booking.com/hotel/hoteladmin/extranet_ng/manage/search_reservations.html?source=nav&upcoming_reservations=1&hotel_id=554570&lang=xu&ses=850ac26632d089624d31f7080dec6c83&date_from=2022-05-13&date_to=2022-05-14&date_type=arrival'
 
+        elif number == 3:
+            return 'https://www.vrbo.com/rm/proxies/v2/conversations/export?afterDate=2022-06-10&beforeDate=2022-07-15&csrfToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjEwY2IyMzE4NGZhZWNiN2ExN2FmMzdlN2NmMzJjOTlkMjFjYTFkMmVhMGRkMmQyNjRkYTBlMWU0NmI0YzY0ZjM5OGNiM2E4MTJmZDMyNTMxNDk0ZmZhODg1NWE4MGMyODkyMzM5MjNmYTlkZGZlYTNjNmM4MjkzMDU1ZTM1ODhjZjBkOGUxZjE1ZDQxMTEwMTcyYzRmMWMwZWVkMzg1ZTY3ZjdmMjhjNGI1YjNlODQ2Nzg3ZDVhOGI3YjJmZGI3OWJiYTE0NDFjY2YwNTg1IiwiaWF0IjoxNjU0NzMyMjM4LCJleHAiOjE2NTUzMzcwMzh9.njbcO9AQLoZlqbwkF80TcQdPo1yfH1aMbnLLzvEZw7U&druid=&reservations=true&site=homeaway_nz&status=RESERVATION_DOWNLOADABLE'
 
     login()
 
+Main()
